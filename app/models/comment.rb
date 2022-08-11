1106dca2f584b3ccf8bee2be14aed4f79654a048
alvinmasaba <alvinmasaba@gmail.com>
2022-08-11 13:30:03 -0400

    creates visibility concern includes it in comment and article models to remove duplication.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,15 +1,5 @@
 class Comment < ApplicationRecord
+  include Visible
+
   belongs_to :article
-
-  validates :commenter, presence: true
-  validates :body, presence: true, length: {
-    minimum: 10 }
-  
-  VALID_STATUSES = ['public', 'private', 'archived']
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  def archived?
-    status == 'archived'
-  end
 end
