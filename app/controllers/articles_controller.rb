23020a253da105763f439b67e1322ee56154668a
alvinmasaba <alvinmasaba@gmail.com>
2022-08-11 14:00:26 -0400

    adds authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "alvin", 
+  password: "secret", except: [:index, :show]
+  
   def index
     @articles = Article.all
   end
