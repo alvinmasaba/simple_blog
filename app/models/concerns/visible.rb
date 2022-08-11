803d36e0ffff4d01d46d93b61e009e4294c64a24
alvinmasaba <alvinmasaba@gmail.com>
2022-08-11 13:46:53 -0400

    adds public count method.

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -4,7 +4,13 @@ module Visible
   VALID_STATUSES = ['public', 'private', 'archived']
 
   included do
-    valildates :status, inclusion: { in: VALID_STATUSES }
+    validates :status, inclusion: { in: VALID_STATUSES }
+  end
+
+  class_methods do
+    def public_count
+      where(status: 'public').count
+    end
   end
   
   def archived?
