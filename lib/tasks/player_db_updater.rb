434e22da5141931d81a97100b806af812387f5b4
funkyman3121 <alvinmasaba@gmail.com>
2022-09-15 14:03:20 -0400

    Deleted secret

--- lib/tasks/player_db_updater.rb
+++ lib/tasks/player_db_updater.rb
@@ -6,7 +6,7 @@ class Spreadsheet
   end
 
   def load_spreadsheet
-    session = GoogleDrive::Session.from_service_account_key(Rails.root + "app/assets/api/client_secret.json")
+    session = GoogleDrive::Session.from_service_account_key(Rails.root + "app/assets/api/client_secret1.json")
     spreadsheet = session.spreadsheet_by_title("IGNBA Roster 2")
 
     spreadsheet.worksheets.first
