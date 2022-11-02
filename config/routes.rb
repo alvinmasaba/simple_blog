d34934b05de187badc3b343d2b7a501532840f4b
funkyman3121 <alvinmasaba@gmail.com>
2022-11-02 16:13:07 -0400

    Creates user controller and show view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
+  resources :users, :only => [:show]
+
   resources :teams do
     resources :players do
       resources :contracts
